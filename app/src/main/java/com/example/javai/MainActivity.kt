08e11721edfdf5ca182561b63c7afda061682dce
cna <cna@me.com>
2022-06-25 17:19:25 +0430

    just for a training

---
+++ app/src/main/java/com/example/javai/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.javai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.javai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

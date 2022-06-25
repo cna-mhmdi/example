704d890b577523372175e6619313ae03d3893af2
cna <cna@me.com>
2022-06-25 20:08:37 +0430

    just for a training

--- app/src/main/java/com/example/javai/MainActivity.kt
+++ app/src/main/java/com/example/javai/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.example.javai
 
-import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val webView = WebView(this)
-
-        webView.settings.javaScriptEnabled=true
-
-        setContentView(webView)
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://google.com")
+        setContentView(R.layout.activity_main)
     }
 }

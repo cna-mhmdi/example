6cf2fd39f3aabce14c3b4e16e73ab773f2b0c1a7
cna <cna@me.com>
2022-06-26 13:06:11 +0430

    new app

--- app/src/main/java/com/example/javai/MainActivity.kt
+++ app/src/main/java/com/example/javai/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.javai
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,22 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.Enter_Button)?.setOnClickListener {
+        findViewById<Button>(R.id.rgb_btn).setOnClickListener {
 
-            val toastText = findViewById<TextView>(R.id.toast_text)
+            val redChannel = findViewById<TextInputEditText>(R.id.red_channel).text.toString()
+            val greenChannel = findViewById<TextInputEditText>(R.id.green_channel).text.toString()
+            val blueChannel = findViewById<TextInputEditText>(R.id.blue_channel).text.toString()
 
-            val firstName = findViewById<TextInputEditText>(R.id.first_Name).text.toString().trim()
+            if (redChannel.length == 2 && greenChannel.length == 2 && blueChannel.length == 2){
 
-            val lastName = findViewById<TextInputEditText>(R.id.last_name).text.toString().trim()
+                val rgbCode = redChannel.plus(greenChannel).plus(blueChannel)
+                val colorAsInt = Color.parseColor("#".plus(rgbCode))
+                findViewById<TextView>(R.id.color_created_display).setBackgroundColor(colorAsInt)
 
-
-            if (firstName.isNotEmpty() && lastName.isNotEmpty()){
-
-                val nameToDisplay = firstName.plus(" ").plus(lastName)
-                toastText.text = " ${getString(R.string.welcome_to_the_app)} $nameToDisplay"
-            }else {
-                toastText.text = " "
-                Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,getString(R.string.please_enter_a_color),Toast.LENGTH_SHORT).show()
             }
         }
     }

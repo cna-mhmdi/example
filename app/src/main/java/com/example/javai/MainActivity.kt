e46f24a73c098cc09ed309f8de88d5c9b1aa88a1
cna <cna@me.com>
2022-06-26 11:28:57 +0430

    some edit

--- app/src/main/java/com/example/javai/MainActivity.kt
+++ app/src/main/java/com/example/javai/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.javai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -18,19 +17,19 @@ class MainActivity : AppCompatActivity() {
 
             val toastText = findViewById<TextView>(R.id.toast_text)
 
-            val firstName = findViewById<TextInputEditText>(R.id.first_Name)?.text.toString().trim()
+            val firstName = findViewById<TextInputEditText>(R.id.first_Name).text.toString().trim()
 
-            val lastName = findViewById<TextInputEditText>(R.id.last_name)?.text.toString().trim()
+            val lastName = findViewById<TextInputEditText>(R.id.last_name).text.toString().trim()
 
 
             if (firstName.isNotEmpty() && lastName.isNotEmpty()){
 
                 val nameToDisplay = firstName.plus(" ").plus(lastName)
-                toastText?.text = " ${getString(R.string.welcome_to_the_app)} $nameToDisplay"
-            }
-            else
+                toastText.text = " ${getString(R.string.welcome_to_the_app)} $nameToDisplay"
+            }else {
                 toastText.text = ""
-                Toast.makeText(this,getString(R.string.please_enter_a_name),Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
